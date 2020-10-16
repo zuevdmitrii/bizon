2d3f0767f629d65296471b303a77199d735b382a
eltonorudzhov <qwetyel@gmailc.com>
2020-10-17 00:52:10 +0300

    bread crumbs

--- src/Main/TasksList.tsx
+++ src/Main/TasksList.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
 import { useTasks } from "./useTasks";
-
+import "./Main.less";
 
 export const TasksList = () => {
   const tasks = useTasks({}, {}, {});
@@ -9,7 +10,14 @@ export const TasksList = () => {
     <div>
       {tasks ? (
         tasks.map((task, index) => {
-          return <div key={index}>{task.title}</div>;
+          return (
+            <div className='tasks_root' key={index}>
+              <Link to={`/task/${task._id}`} className="list__row-wrapper">
+                Открыть
+              </Link>
+              <div className={'taskTitle'} key={index}>{task.title}</div>
+            </div>
+          );
         })
       ) : (
         <div>Загрузка</div>
