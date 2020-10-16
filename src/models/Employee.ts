2755bddc385889ffb28e71688afdde12df8426e8
eltonorudzhov <qwetyel@gmailc.com>
2020-10-16 22:35:38 +0300

    basic mongo models

---
+++ src/models/Employee.ts
@@ -0,0 +1,20 @@
+const { Schema, model } = require("mongoose");
+
+const EmployeeScheme = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  role: String,
+  departmentId: {
+    type: Schema.Types.ObjectID,
+    ref: 'Department',
+    required: true
+  }
+})
+
+module.exports = model("Employee", EmployeeScheme, "employees");
---
+++ src/models/History.ts
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+
+const HistoryScheme = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  assignee: {
+    type: String,
+    required: true,
+  },
+  status: String,
+  creationDate: Date,
+  targetData: Date,
+  taskId: {
+    type: Schema.Types.ObjectID,
+    ref: 'Task',
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  }
+})
+
+module.exports = model("History", HistoryScheme, "history");
