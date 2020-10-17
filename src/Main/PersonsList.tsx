2adae420196fb25a6a3e9788b44bc3342a52372d
eltonorudzhov <qwetyel@gmailc.com>
2020-10-17 22:48:11 +0300

    person-list style

--- src/Main/PersonsList.tsx
+++ src/Main/PersonsList.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { employeeDelete } from "../api/EmployeeApi";
 import { IDataProviderFilter, Logic, Operator } from "../api/IFilter";
 import { Input } from "../Task/Input";
 import { Button } from "./Components/Button";
 import "./PersonsList.less";
 import { usePersons } from "./usePersons";
+
 
 export const PersonsList = (props: {
   setUser?: (id: string, email?: string) => void;
@@ -15,6 +16,8 @@ export const PersonsList = (props: {
   const [filters, setFilters] = useState<IDataProviderFilter | null>();
   const [filterValue, setFilterValue] = useState("");
   const persons = usePersons(filters, {}, {});
+  const history = useHistory();
+
   return (
     <div className="listRoot">
       <Link to={`/person/new`} className="list__row-wrapper">
@@ -54,33 +57,64 @@ export const PersonsList = (props: {
         />
       </div>
       {persons && persons.length ? (
-        persons.map((person, index) => {
-          return (
-            <div className="list_root" key={index}>
-              <Link to={`/person/${person._id}`} className="list__row-wrapper">
-                Открыть
-              </Link>
-              <div className={"listTitle"} key={index}>
-                {person.firstName}
-              </div>
-              <Button
-                caption={"X"}
-                onClick={() => {
-                  employeeDelete(person._id);
-                }}
-              />
-              {props.setUser && (
-                <Button
-                  onClick={() => {
-                    props.setUser(person._id, person.email);
-                    props.onClose();
-                  }}
-                  caption={"Выбрать"}
-                />
-              )}
-            </div>
-          );
-        })
+        <table width={"100%"}>
+          <thead>
+            <tr>
+              <th></th>
+              <th></th>
+              <th>Выполнены</th>
+              <th>В работе</th>
+              <th>Количество</th>
+              <th></th>
+              {props.setUser && <th></th>}
+            </tr>
+          </thead>
+          <tbody>
+            {persons.map((person, index) => {
+              return (
+                <tr className={'personTr'} key={index}>
+                  <td></td>
+                  <td
+                    className={"listTitle"}
+                    onClick={() => {
+                      console.log("there");
+                      history.push(`/person/${person._id}`);
+                    }}
+                  >
+                    <div>
+                      {" "}
+                      {person.firstName + " "} {person.lastName}
+                    </div>
+                    <div className='subText'> {person.role} </div>
+                    <div className='subText'>{person.department}</div>
+                  </td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td>
+                    <Button
+                      caption={"X"}
+                      onClick={() => {
+                        employeeDelete(person._id);
+                      }}
+                    />
+                  </td>
+                  {props.setUser && (
+                    <td>
+                      <Button
+                        onClick={() => {
+                          props.setUser(person._id, person.email);
+                          props.onClose();
+                        }}
+                        caption={"Выбрать"}
+                      />
+                    </td>
+                  )}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       ) : persons ? (
         <div>Список пуст</div>
       ) : (
