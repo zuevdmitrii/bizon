93a3e6d2566a794de5e3c022d05bbcaab2b6d974
Daniil <daniil.denisov@clevercraft.net>
2020-10-17 13:59:26 +0300

    create, update task

--- src/Main/TasksList.tsx
+++ src/Main/TasksList.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 import { useTasks } from "./useTasks";
 import "./Main.less";
+import { Button } from "./Components/Button";
 
 export const TasksList = () => {
   const tasks = useTasks({}, {}, {});
@@ -9,16 +10,23 @@ export const TasksList = () => {
   return (
     <div>
       {tasks ? (
-        tasks.map((task, index) => {
-          return (
-            <div className='list_root' key={index}>
-              <Link to={`/task/${task._id}`} className="list__row-wrapper">
-                Открыть
-              </Link>
-              <div className={'listTitle'} key={index}>{task.title}</div>
-            </div>
-          );
-        })
+        <div>
+          {tasks.map((task, index) => {
+            return (
+              <div className="list_root" key={index}>
+                <Link to={`/task/${task._id}`} className="list__row-wrapper">
+                  Открыть
+                </Link>
+                <div className={"listTitle"} key={index}>
+                  {task.title}
+                </div>
+              </div>
+            );
+          })}
+          <Link to={`/task/new/`} className="list__row-wrapper">
+            <Button onClick={() => {}} caption={"Создать задачу"} />
+          </Link>
+        </div>
       ) : (
         <div>Загрузка</div>
       )}
